a207866f3ca394141f29918d27099aed0b661c91
VinsDev <vinsdev185@gmail.com>
2024-08-02 13:00:33 +0100

    m

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: false,
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Render PostgreSQL database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  });
+} else {
+  // Local MySQL database
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  });
+}
 
 sequelize.authenticate()
   .then(() => {
