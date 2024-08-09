5f874d0c9c24e12cfaaa8671ee806e7234a8f4cf
VinsDev <vinsdev185@gmail.com>
2024-08-09 12:37:20 +0100

    image uploads

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./resultsphere-c958c-firebase-adminsdk-pz5jv-1c7613a76a.json'); // Path to your service account JSON file
+const serviceAccount = require('./serviceKey.json');
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
